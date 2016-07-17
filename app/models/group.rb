f08a5efca3ec087ad3b0ed3df371831e57d433c4
josafa martins <desenvolvedorjosafa@gmail.com>
2016-07-17 20:40:36 -0300

    criado metodo my_groups dentro de User

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,12 @@
 class Group < ActiveRecord::Base
   belongs_to :user
   
+  has_many :user_groups
   has_many :users, :through => :user_groups
   has_many :messages
+
+  def validates_groups	
+  	return UserGroup.where().count < 7 ? true : false
+  end
+
 end
