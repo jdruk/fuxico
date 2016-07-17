4a109f4e9bc4bf6d9d715ed2b6c15b4d8d83dcc0
Guilherme <guilhermefeitosa66@gmail.com>
2016-07-17 17:41:33 -0300

    setted semantic-ui lib

--- Gemfile
+++ Gemfile
@@ -20,3 +20,4 @@ group :development do
 end
 
 gem 'devise'
+gem 'carrierwave'
--- python-apps/client.py
+++ python-apps/client.py
@@ -6,8 +6,8 @@ view = webkit.WebView()
 win = gtk.Window(gtk.WINDOW_TOPLEVEL)
 
 win.connect("destroy", gtk.main_quit)
-win.set_keep_above(True)
-win.fullscreen()
+#win.set_keep_above(True)
+#win.fullscreen()
 
 settings = view.get_settings()
 settings.set_property('enable-default-context-menu', False)
@@ -20,6 +20,6 @@ scroller.add(view)
 
 win.add(view)
 win.show_all()
-view.open("http://localhost:3000/login")
+view.open("http://localhost:3000")
 
 gtk.main()
